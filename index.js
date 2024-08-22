e89a413f7846d55df8e71f7abc7c5d7794b99d84
facundoverdasco <166016911+facundoverdasco@users.noreply.github.com>
2024-08-22 08:30:26 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+//actividad 1
+let fyv = []
+let kilos = []
+
+//actividad 1A
+
+for (let index = 0; index < 6; index++) {
+  fyv.push = prompt("ingrese una fruta")
+  kilos.push = prompt ("ingrese cuantos kilos hay de la fruta que ingreso")
+}
+
+//actividad 1C
+
+for (let index = 0; index < fyv.length; index++) {
+    if (fyv[index]== "tomate") {
+    
+  console.log(fyv.indexOf = fyv[index])
+    } else {
+        console.log("no hay tomate")
+    }
+    
+}
+
+//actividad 1D
+
+kilos[0]= kilos[0] +20
+kilos[1]= kilos[1] +20
+
+//actividad 1E
+for (let index = 0; index < fyv.length; index++) {
+   console.log(fyv[index])
+}
+//acividad 1F
+
+for (let index = 0; index < kilos.length; index++) {
+   if (kilos[index] >20) {
+       console.log(kilos[index])
+   }
+    
+}
